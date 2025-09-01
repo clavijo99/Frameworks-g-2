737a87a9cca7e1005ac855766fbe77d23c53a8db
clavijo99 <bclavijogomez@gmail.com>
2025-09-01 10:53:44 -0500

    Refactor Header component to improve structure and add navigation links

--- clases/navegacion/src/components/ui/Header/Header.jsx
+++ clases/navegacion/src/components/ui/Header/Header.jsx
@@ -1,4 +1,8 @@
+
+// componente para manejar las imagenes en next - esta optimizado
 import Image from "next/image"
+// componente para los links
+import Link from "next/link"
 
 
 const banner = '/banner.webp'
@@ -7,8 +11,19 @@ const banner = '/banner.webp'
 
 export default function Header(){
     return (
-        <header className="" >
+        <header className=" flex flex-col w-full h-[500px] bg-white relative " >
             <Image src={banner}  fill />
+
+            <nav className=" flex justify-between text-black  relative">
+                <Link href='/' className="mr-10" > Logo de mi Tienda </Link>
+                <div>
+                    <Link href='/' >Home</Link>
+                    <Link>Servicios</Link>
+                    <Link>Contacto</Link>
+                    <Link>Informacion</Link>
+                </div>
+            </nav>
+
         </header>
     )
 }
