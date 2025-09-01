ef3f014bcd323aca6ae32d947d1817565f6fe35f
clavijo99 <bclavijogomez@gmail.com>
2025-09-01 11:31:58 -0500

    Refactor Header component for improved structure and readability

--- clases/navegacion/src/components/ui/Header/Header.jsx
+++ clases/navegacion/src/components/ui/Header/Header.jsx
@@ -9,18 +9,23 @@ const banner = '/banner.webp'
 
 
 
-export default function Header(){
+export default function Header() {
     return (
         <header className=" flex flex-col w-full h-[500px] bg-white relative " >
-            <Image src={banner}  fill />
+            <Image src={banner} fill />
 
-            <nav className=" flex justify-between text-black  relative">
-                <Link href='/' className="mr-10" > Logo de mi Tienda </Link>
+            <nav className=" flex justify-between text-black  relative pt-6 px-4 ">
+                <div className=" flex ">
+                    <Link href='/' className="mr-10" > Logo de mi Tienda </Link>
+                    <div className=" hidden sm:flex " >
+                        <Link className=" mr-4 hover:text-red-500 hover:border-b-2" href='/' >Home</Link>
+                        <Link className=" mr-4 hover:text-red-500 hover:border-b-2" href='/services'>Servicios</Link>
+                        <Link className=" mr-4 hover:text-red-500 hover:border-b-2" href='/contact' >Contacto</Link>
+                        <Link className=" mr-4 hover:text-red-500 hover:border-b-2" href='/about' >Informacion</Link>
+                    </div>
+                </div>
                 <div>
-                    <Link href='/' >Home</Link>
-                    <Link href='/services'>Servicios</Link>
-                    <Link href='/contact' >Contacto</Link>
-                    <Link href='/about' >Informacion</Link>
+                    <Link className="hover:border-b-2 hover:text-red-500 hover:border-red-600" href='/login' >Iniciar sesion</Link>
                 </div>
             </nav>
 
