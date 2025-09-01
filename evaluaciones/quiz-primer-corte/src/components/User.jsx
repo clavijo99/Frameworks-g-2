3c6953130a332e654208f1f0349052878f0d86b4
clavijo99 <bclavijogomez@gmail.com>
2025-09-01 10:13:34 -0500

    Fix User component prop destructuring and correct prop name in Home component

--- evaluaciones/quiz-primer-corte/src/components/User.jsx
+++ evaluaciones/quiz-primer-corte/src/components/User.jsx
@@ -1,5 +1,5 @@
-Function User(nombre, apellido, edad){
-    return {
+export default function User({nombre, apellido, edad}){
+    return (
         <h1 className=" text-2xl hover:text-red-400 "> Hola {nombre} {apellido}  tu edad es {edad}</h1>
-    }
+    )
 }
