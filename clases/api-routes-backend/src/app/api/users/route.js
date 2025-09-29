ff9c09ce96dfd565067cb7a34cb18b56bc42e504
clavjo99 <bclavijogomez@gmail.com>
2025-09-29 10:55:33 -0500

    Refactor user API route to fetch users from the database instead of a static array

--- clases/api-routes-backend/src/app/api/users/route.js
+++ clases/api-routes-backend/src/app/api/users/route.js
@@ -1,60 +1,11 @@
-const users = [
-    {
-        nombre: "Juan",
-        apellido: "Pérez",
-        email: "juan.perez@example.com",
-        telefono: "3001234567",
-        contraseña: "password123"
-    },
-    {
-        nombre: "Maria",
-        apellido: "Gomez",
-        email: "maria.gomez@example.com",
-        telefono: "3109876543",
-        contraseña: "securepass456"
-    }
-];
+import pool from "@/lib/db";
 
-// http:://localhost:3000/api/{carpeta "users"}
-// GET, POST , DELETE, PATCH, PUT
+
 export async function GET() {
-
-    // INSTANCE de la clase Response ( body , cabezeras )
-    // JSON.stringfy me permite convertir un objeto a un string
-    return new Response(JSON.stringify(users), {
-        // status es el codigo de la peticion que va desde 100 al 500
-        status: 200,
-        headers: {
-            // vamos a manejas respuestas en en formato json
-            'Content-Type': 'application/json'
-        }
-    });
-}
-
-
-// agregue el atributo req --> tienen toda la informacion de mi peticion
-export async function POST(req) {
-    // para obtener los datos del body debemos procesar el req ya que es async
-    const body = await req.json();
-
-    
-    const { nombre, apellido, email, telefono, contraseña } = body;
-
-    const user = {
-        nombre, apellido, email, telefono, contraseña
-    };
-
-    users.push(user)
-
-    return new Response(
-        JSON.stringify({
-            "message": "Usuario creado",
-            "usuario": user
-        }), {
-        status: 201,
-        headers: {
-            'Content-Type': 'application/json'
-        }
+    try {
+        const result  = await pool.query('SELECT * FROM users')
+    } catch
+    (error) {
+        return new Response('Error interno del servidor', { status: 500 })
     }
-    )
 }
