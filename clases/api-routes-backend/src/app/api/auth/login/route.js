cc650a64fdb5c0149607c77c1505ba825b7ad693
clavjo99 <bclavijogomez@gmail.com>
2025-10-01 12:04:35 -0500

    Add password validation to login API

--- clases/api-routes-backend/src/app/api/auth/login/route.js
+++ clases/api-routes-backend/src/app/api/auth/login/route.js
@@ -2,6 +2,11 @@ import pool from '@/lib/db';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+
+const SECRET_KEY_ACCESS_TOKEN = 'MY-KEY-ACCESS-T'
+const SECRET_KEY_REFRESH = 'MY-KEY-REFRESH-T'
+
+
 export async function POST(req) {
     
     try{
@@ -12,6 +17,15 @@ export async function POST(req) {
         )
 
         if( verifyUser.rows.length  > 0){
+            const users = verifyUser.rows[0];
+
+            const isPasswordValidate = await bcrypt.compare(password, users.password)
+
+            if(isPasswordValidate){
+
+            }
+
+            return Response.json({message: "Las credenciales son incorrectas"})
 
         }
 
@@ -21,4 +35,25 @@ export async function POST(req) {
         console.log(error)
         return new Response('Error interno del servidor', { status: 500 })
     }
+}
+
+
+export function generateToken(user) {
+    const accessToken = jwt.sign(
+        // informacion del usuario
+        {"id": user.id, "name": user.name, "email": user.email},
+        // llave secreta
+        SECRET_KEY_ACCESS_TOKEN,
+        // configuraciones
+        {expiresIn: '30M'}
+    )
+
+        const refreshToken = jwt.sign(
+        // informacion del usuario
+        {"id": user.id, "name": user.name, "email": user.email},
+        // llave secreta
+        SECRET_KEY_REFRESH,
+        // configuraciones
+        {expiresIn: '1h'}
+    )
 }
