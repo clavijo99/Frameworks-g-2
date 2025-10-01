82635f30b153400e3408c40d49138e082d5b2807
clavjo99 <bclavijogomez@gmail.com>
2025-10-01 11:08:43 -0500

    Add email verification to user registration API

--- clases/api-routes-backend/src/app/api/auth/register/route.js
+++ clases/api-routes-backend/src/app/api/auth/register/route.js
@@ -2,9 +2,19 @@ import pool from '@/lib/db';
 import bcrypt from 'bcrypt';
 
 
-export default async function POST(req) {
+export  async function POST(req) {
     try{
         const { name, email, password } = await req.json();
+
+        const emailVerify = await pool.query(
+            "SELECT * FROM users WHERE email = $1", [email]
+        )
+
+        if(  emailVerify.rows.length > 0 ){
+            return Response.json({
+                message: "El Correo ya se encuentra registrado"
+            })
+        }
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
